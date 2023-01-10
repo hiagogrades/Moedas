8c2210b1bdcdbbd20dad4e83d6bda1e86111e520
Hiago Grades <hiagogrades@hotmail.com>
2023-01-10 19:13:41 -0300

    Inicia projeto de exemplo para manipulação de moedas

---
+++ FormatandoMoedas/FormatandoMoedas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace FormatandoMoedas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 10.25m;
+
+            Console.WriteLine(valor);
+
+            //Creando uma especificação de cultura
+            Console.WriteLine(valor.ToString(
+                "C", //Formato de Curence (moeda), converte para moeda real, colocando o $
+                CultureInfo.CreateSpecificCulture("pt-BR")));
+
+            Console.WriteLine(valor.ToString(
+                "C", //Formato de Curence (moeda), converte para moeda dolar, colocando o $
+                CultureInfo.CreateSpecificCulture("es-US")));
+        }
+    }
+}
